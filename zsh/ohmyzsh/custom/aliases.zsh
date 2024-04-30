a77c5266b3f530ab0c1217dc140ba6938dc3266b
rslangl <fjellape1447@protonmail.com>
2024-04-30 21:27:21 +0200

    feat: working X config and cleaned up zsh

--- zsh/ohmyzsh/custom/aliases.zsh
+++ zsh/ohmyzsh/custom/aliases.zsh
@@ -1,4 +1,4 @@
 alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts'
-alias pubip='curl http://ipecho.net/plan; echo'
 alias vc='nvim $HOME/.config/nvim'
 alias v='nvim .'
+alias pubip='curl http://ipecho.net/plan; echo'
--- zsh/ohmyzsh/custom/kubeconfig.zsh
+++
@@ -1,4 +0,0 @@
-function merge_config() {
-  export KUBECONFIG=$XDG_CONFIG_HOME/kube/config:$(find . -type f | tr '\n' ':')
-  kubectl config view --flatten > $XDG_CONFIG_HOME/kube/config
-}
