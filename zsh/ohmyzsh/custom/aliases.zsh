a48914addfdbc01faf7a1dbf8b524184d2785e62
rslangl <fjellape1447@protonmail.com>
2024-12-28 18:34:45 +0100

    feat: some tidying and moving away from dwm

--- zsh/ohmyzsh/custom/aliases.zsh
+++ zsh/ohmyzsh/custom/aliases.zsh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts'
 
 alias vc='nvim $HOME/.config/nvim'
@@ -10,3 +11,9 @@ alias sbt='sbt -ivy "$XDG_DATA_HOME"/ivy2 -sbt-dir "$XDG_DATA_HOME"/sbt'
 alias nvidia-settings='nvidia-settings --config="$XDG_CONFIG_HOME"/nvidia/settings'
 
 
+=======
+alias wget="wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
+alias sbt="sbt -ivy $XDG_DATA_HOME/ivy2 -sbt-dir $XDG_DATA_HOME/sbt"
+alias mvn="mvn -gs $XDG_CONFIG_HOME/maven/settings.xml"
+alias nvidia-settings="nvidia-settings --config=$XDG_CONFIG_HOME/nvidia/settings"
+>>>>>>> b1d9b7a (feat: some tidying and moving away from dwm)
