8b37aa10911a736ada9419542d69f2991bd68ed5
rslangl <fjellape1447@protonmail.com>
2025-12-30 23:10:57 +0100

    refactor: set XDG dirs and ZDOTDIR via environment.d

--- run_once_0-xdg-dirs.sh
+++ run_once_0-xdg-dirs.sh
@@ -2,23 +2,8 @@
 
 set -e
 
-sudo echo "export ZDOTDIR="$HOME"/.config/zshenv"
-
-sudo mkdir -p /etc/profile.d
-sudo touch /etc/profile.d/xdg_dirs.sh
-
-cat <<EOF | sudo tee /etc/profile.d/xdg_dirs.sh >/dev/null
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_STATE_HOME="$HOME/.local/state"
-
-export XDG_DATA_DIRS=/usr/local/share:/usr/share
-export XDG_CONFIG_DIRS=/etc/xdg
-
-export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
-export BASH_ENV="$XDG_CONFIG_HOME/bash/.bashrc"
+sudo touch /etc/environment.d/zsh.conf
+sudo cat <<EOF | sudo tee /etc/environment.d/zsh.conf >/dev/null
+ZDOTDIR=%h/.config/zsh
 EOF
-
-sudo chmod 644 /etc/profile.d/xdg_dirs.sh
 
